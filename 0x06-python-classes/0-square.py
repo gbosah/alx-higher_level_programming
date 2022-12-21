586a03de7e2a2cbe8b46790fda1dca59e4bc598a
gbosah <gbosah@gmail.com>
2022-12-21 18:48:37 +0000

    python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Defines a Square"""
 
-"""This class don't do anything for the moment"""
-
-
 
 
 class Square:
+
+"""This class don't do anything for the moment"""
     pass
